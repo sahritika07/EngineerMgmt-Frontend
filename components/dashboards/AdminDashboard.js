9422b2111cdc2c172784e594eedd56c32e6e7ee0
Ritika Sah <ritikasah731@gmail.com>
2025-07-13 02:43:19 +0530

    Changes

--- components/dashboards/AdminDashboard.js
+++ components/dashboards/AdminDashboard.js
@@ -352,6 +352,7 @@ export default function AdminDashboard({ user }) {
           </div>
         </div>
 
+
         {/* Upcoming Deadlines */}
         <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6">
           <div className="flex items-center justify-between mb-4">
